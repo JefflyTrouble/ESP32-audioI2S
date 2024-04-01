243394a0a01b0051bb6007ad0476dfc7064ff5e6
Jeffly <128430768+JefflyTrouble@users.noreply.github.com>
2024-04-02 03:53:10 +1000

    Increased reserved and frame buffer size for FLAC to support 24 bit

--- src/Audio.h
+++ src/Audio.h
@@ -116,7 +116,7 @@ protected:
     size_t   m_writeSpace       = 0;
     size_t   m_dataLength       = 0;
     size_t   m_resBuffSizeRAM   = 2048;     // reserved buffspace, >= one wav  frame
-    size_t   m_resBuffSizePSRAM = 4096 * 4; // reserved buffspace, >= one flac frame
+    size_t   m_resBuffSizePSRAM = 4096 * 8; // reserved buffspace, >= one flac frame
     size_t   m_maxBlockSize     = 1600;
     uint8_t* m_buffer           = NULL;
     uint8_t* m_writePtr         = NULL;
@@ -488,7 +488,7 @@ private:
     const size_t    m_frameSizeWav    = 2048;
     const size_t    m_frameSizeMP3    = 1600;
     const size_t    m_frameSizeAAC    = 1600;
-    const size_t    m_frameSizeFLAC   = 4096 * 4;
+    const size_t    m_frameSizeFLAC   = 4096 * 8;
     const size_t    m_frameSizeOPUS   = 1024;
     const size_t    m_frameSizeVORBIS = 4096 * 2;
     const size_t    m_outbuffSize     = 4096 * 2;
